84b69f451f93bd8a66afcc2543208415d9aa419c
tony <tony@gmail.com>
2022-07-11 15:17:41 +0400

    fixed

---
+++ src/lang/en.js
@@ -0,0 +1,25 @@
+export default {
+  message: {
+    'hello': 'hello, world',
+    'msg': 'point out'
+  },
+  member: {
+    menu: 'member Manager',
+    list: {
+      menu: 'member List',
+      table: {
+        id: 'member id',
+        username: 'username',
+        avatar:'avatar'
+      }
+    },
+    flow: {
+      menu: 'member Flow',
+      table: {
+        id: 'member id',
+        username: 'username',
+        avatar:'avatar'
+      }
+    }
+  }
+}
